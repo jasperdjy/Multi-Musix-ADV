a15551abe7cd9429b2f28190192db35e2f9100ef
jasperdjy <ggrggegwrwg@gmail.com>
2024-05-31 23:02:25 +0600

    Update help.js

--- basicCommands/help.js
+++ basicCommands/help.js
@@ -17,7 +17,7 @@ module.exports = {
         url: 'https://discord.gg/v2NJhUg2Jc'
     })
      
-      .setDescription(`__**STATS :**__\n\n> **📊 Bot in servers:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **👑 Made By [JasperSkye] (discord.gg/H7thnM6a)**\n\n__**COMMANDS :**__ `)
+      .setDescription(`__**STATS :**__\n\n> **📊 Bot in servers:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **👑 Made By Jasper Skye n\n__**COMMANDS :**__ `)
       .addFields(
       
         {
