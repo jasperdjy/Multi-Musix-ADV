c60d12f49c16387bdbdfe5e5b0e1e9f357d820f3
jasperdjy <ggrggegwrwg@gmail.com>
2024-06-01 14:11:54 +0600

    Update owner.js

--- utilityCommands/owner.js
+++ utilityCommands/owner.js
@@ -4,12 +4,12 @@ module.exports = {
     name: 'owner',
     description: 'Bot owner info',
     execute(message, args) {
-        const youtubeLink = 'https://www.youtube.com/@GlaceYT';
-        const InstagramLink = 'https://www.instagram.com/glaceytt';
+        const youtubeLink = 'https://www.youtube.com/channel/UCm2LpjddjPz4QL0DKflzXLw';
+        const InstagramLink = 'https://www.instagram.com/jasper_skye_0/';
         const embed = new EmbedBuilder()
             .setColor('#FFFFFF')
             .setTitle(' 🫅 Owner Info')
-            .setDescription(`__**About me**__:\n 🤖 Myself Shiva aka GlaceYT. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [GlceYT](${youtubeLink})\n 💙 [GlaceYTT](${InstagramLink})`)
+            .setDescription(`__**About me**__:\n 🤖 Myself Jasper Skye. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [Jasper](${youtubeLink})\n 💙 [Jasper-skye](${InstagramLink})`)
             .setTimestamp();
 
 
